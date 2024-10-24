4e3ae64d45c835d5398eb71537768e18cb32cea4
pc <pc@DESKTOP-78M62IA>
2024-10-24 20:33:12 +0300

    Add project files.

---
+++ PatikaKütüphane/Book.cs
@@ -0,0 +1,13 @@
+﻿public class Kitap
+{
+    public string Ad; // Kitap adı
+    public string YazarAdi; // Yazar adı
+    public int SayfaSayisi; // Sayfa sayısı
+
+    public Kitap(string ad, string yazarAdi, int sayfaSayisi)
+    {
+        Ad = ad; // Kitap adını ayarlıyoruz
+        YazarAdi = yazarAdi; // Yazar adını ayarlıyoruz
+        SayfaSayisi = sayfaSayisi; // Sayfa sayısını ayarlıyoruz
+    }
+}
---
+++ PatikaKütüphane/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Yeni bir kitap oluştur
+        Kitap kitap = new Kitap("Aylin", "Ayşe", 398);
+
+        // Kitap bilgilerini yazdır
+        Console.WriteLine("Kitap Adı: " + kitap.Ad);
+        Console.WriteLine("Yazar: " + kitap.YazarAdi);
+        Console.WriteLine("Sayfa Sayısı: " + kitap.SayfaSayisi);
+    }
+}
